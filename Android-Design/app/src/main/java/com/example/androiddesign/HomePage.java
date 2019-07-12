6bddc16e1a096e7b49df92d901fe5742d37bb833
O-direction <2537169087@qq.com>
2019-07-13 00:23:57 +0800

    7.13-0.21

--- Android-Design/app/src/main/java/com/example/androiddesign/HomePage.java
+++ Android-Design/app/src/main/java/com/example/androiddesign/HomePage.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hb.dialog.myDialog.MyAlertInputDialog;
+
+import cn.jpush.im.android.api.ContactManager;
 import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.api.BasicCallback;
+
+import static cn.jpush.im.android.api.ContactManager.sendInvitationRequest;
 
 public class HomePage extends AppCompatActivity {
 
@@ -27,6 +33,7 @@ public class HomePage extends AppCompatActivity {
         talk = (TextView)findViewById(R.id.talk);
         friends = (TextView)findViewById(R.id.friends);
         me = (TextView)findViewById(R.id.me);
+        final HomePage that = this;
 
         Drawable D_talk = getResources().getDrawable(R.drawable.talk);
         D_talk.setBounds(0, 0, 40, 40);//第一0是距左边距离，第二0是距上边距离，40分别是长宽
@@ -44,7 +51,32 @@ public class HomePage extends AppCompatActivity {
         add_friend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                final MyAlertInputDialog myAlertInputDialog = new MyAlertInputDialog(that).builder()
+                        .setTitle("请输入对方的用户名")
+                        .setEditText("");
+                myAlertInputDialog.setPositiveButton("确认", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String res = myAlertInputDialog.getResult();
+                        myAlertInputDialog.dismiss();
+                        ContactManager.sendInvitationRequest(res,"", "hello", new BasicCallback(){
+                            @Override
+                            public void gotResult(int i, String s) {
+                                if(i==0){
+                                    Toast.makeText(getApplicationContext(),"发送成功",Toast.LENGTH_LONG).show();
+                                }else{
+                                    Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                }).setNegativeButton("取消", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myAlertInputDialog.dismiss();
+                    }
+                });
+                myAlertInputDialog.show();
             }
         });
 
